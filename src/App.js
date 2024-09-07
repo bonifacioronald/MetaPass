81d546f8ef8d7d5df7049d9365ef8efecacf9480
bonifacioronald <bonifacioronald99@gmail.com>
2024-09-07 21:15:59 +0800

    Initial wallet and account front end setup

--- src/App.js
+++ src/App.js
@@ -14,40 +14,18 @@ import MetaPass from './abis/MetaPass.json'
 import config from './config.json'
 
 function App() {
-  const [provider, setProvider] = useState(null)
-  const [account, setAccount] = useState(null)
 
-  const [metaPass, setMetaPass] = useState(null)
-  const [occasions, setOccasions] = useState([])
+  const [account, setAccount] = useState(null);
 
-  const [occasion, setOccasion] = useState({})
-  const [toggle, setToggle] = useState(false)
-
-  const loadBlockchainData = async () => {
-    const provider = new ethers.providers.Web3Provider(window.ethereum)
-    setProvider(provider)
-
-    const network = await provider.getNetwork()
-    const metaPass = new ethers.Contract(config[network.chainId].MetaPass.address, MetaPass, provider)
-    setMetaPass(metaPass)
-
-    const totalOccasions = await metaPass.totalOccasions()
-    const occasions = []
-
-    for (var i = 1; i <= totalOccasions; i++) {
-      const occasion = await metaPass.getOccasion(i)
-      occasions.push(occasion)
-    }
-
-    setOccasions(occasions)
-
+  const loadBlockchainData = async() => {
+    //Refresh Account
     window.ethereum.on('accountsChanged', async () => {
       const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
       const account = ethers.utils.getAddress(accounts[0])
       setAccount(account)
     })
   }
-
+  
   useEffect(() => {
     loadBlockchainData()
   }, [])
@@ -55,37 +33,12 @@ function App() {
   return (
     <div>
       <header>
-        <Navigation account={account} setAccount={setAccount} />
-
-        <h2 className="header__title"><strong>Event</strong> Tickets</h2>
+        <Navigation account={account} setAccount={setAccount}/>
+        <h2 className='header__title'><b>Event</b> Tickets</h2>
       </header>
 
-      <Sort />
-
-      <div className='cards'>
-        {occasions.map((occasion, index) => (
-          <Card
-            occasion={occasion}
-            id={index + 1}
-            metaPass={metaPass}
-            provider={provider}
-            account={account}
-            toggle={toggle}
-            setToggle={setToggle}
-            setOccasion={setOccasion}
-            key={index}
-          />
-        ))}
-      </div>
-
-      {toggle && (
-        <SeatChart
-          occasion={occasion}
-          metaPass={metaPass}
-          provider={provider}
-          setToggle={setToggle}
-        />
-      )}
+      <h1>MetaPass</h1>
+      <p>{account}</p>
     </div>
   );
 }
